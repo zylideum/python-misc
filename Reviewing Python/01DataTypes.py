ee304f0d3eb3fdd88092bf03b87459d3fb6bcf72
0xA <43623870+zylideum@users.noreply.github.com>
2022-03-21 22:57:32 -0700

    Import 01

---
+++ Reviewing Python/01DataTypes.py
@@ -0,0 +1,21 @@
+# 0xA / Zylideum
+# 02/23/2022
+# Project: Reviewing Python
+
+a = 3
+b = 4.5
+c = True
+d = 'Hello'
+e = 'a'
+
+print("There are integers, floats, booleans, strings, and characters.")
+print('\'', a,  '\'', "is an integer.")
+print('\'', b, '\'', "is a float.")
+print('\'', c, '\'', "is a boolean.")
+print('\'', d, '\'', "is a string.")
+print('\'', e, '\'', "is a character.")
+
+# Future Challenge
+# Remove the spaces between the apostrophes and the values.
+# Concatenation works, but is it better? 
+# Evaluate performance benefit of , vs. +
